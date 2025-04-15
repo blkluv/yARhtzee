5528cc9bcfc678932b968a4cc55c8eee8637064d
platane <me@platane.me>
2025-04-15 09:26:11 +0200

    🧹 clean up dependencies

--- src/XR8Canvas/useXR8.ts
+++
@@ -1,8 +0,0 @@
-import { loadXR8 } from "./getXR8";
-import { useAsset } from "use-asset";
-
-/**
- * load XR8 sdk
- */
-export const useXR8 = (xr8ApiKey: string | undefined) =>
-  useAsset(loadXR8, xr8ApiKey);
